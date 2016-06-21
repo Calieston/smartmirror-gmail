aea3210ffb74b2d980bc50aa83dc0a83500cb92d
Calieston <calieston@hotmail.com>
2016-06-21 22:47:35 +0200

    Refactored module

--- index.js
+++ index.js
@@ -2,27 +2,33 @@
 
 var express = require('express');
 var app = express();
-var util = require('util');
 var controller = require('./app/controller');
+var fs = require('fs');
 
 
 app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
-  let user = '******************'
-  let password = '******************'
+  let user = '*****'
+  let password = '*****'
   let host = 'imap.gmail.com'
+  let pckg = fs.readFileSync('package.json');
+  pckg = JSON.parse(pckg);
+
+  let widget = {};
+  widget._id = '1234567890';
+  widget.size = pckg.smartmirror.size[0];
 
   controller.get({
       user: user,
       password: password,
       host: host
     })
-    .then((mails) => {
-      res.render('app/view.jade', {
-        title: 'Mail Information',
-        mails: mails
+    .then((data) => {
+      res.render('./index.jade', {
+        data: data,
+        widget: widget,
       });
     });
 });
